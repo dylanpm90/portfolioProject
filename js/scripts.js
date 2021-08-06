6b01bc0ad24e5ecb4d8e0a26004219d015d197a9
Dylan MacKay <dylan.p.mackay@gmail.com>
2021-08-06 16:31:01 -0500

    modals and footers and links

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$("#wateringBtn").on("click", function(){
+    $('#wateringModal').modal('show');
+});
+
+$("#pottingBtn").on("click", function(){
+    $('#pottingModal').modal('show');
+});
+
+$("#soilBtn").on("click", function(){
+    $('#soilModal').modal('show');
+});
